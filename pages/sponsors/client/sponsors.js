59037f3c4e6ef2ee14cd522e9190e36990668879
Dtelano1 <41086573+Dtelano1@users.noreply.github.com>
2018-07-10 14:21:29 -0700

    AppDesign

--- pages/sponsors/client/sponsors.js
+++ pages/sponsors/client/sponsors.js
@@ -1,6 +1,6 @@
 const ourSponsors = [
  {name:"Google",amt:"$50 million", date:"1/1/2018", ceo:"Jimmy"},
- {name:"Apple",amt:"$20 million", date:"2/2/2018", ceo},
+ {name:"Apple",amt:"$20 million", date:"2/2/2018", ceo"Ryan"},
  {name:"My Mom",amt:"$10", date:"3/3/2018", ceo"Naomi"},
  {name:"My Dad",amt:"$9.99", date: "4/4/2018", ceo"Dean"},
  {name:"Facebook",amt:"$40 million", date: "5/5/2018", ceo"Mark"},
