a69359e69de5655ccf8138226d2758dbd61babd0
William Martin-Gleason <williammartingleason@gmail.com>
2018-07-11 11:44:32 -0400

    Mechanic Profile
    Mechanics will be able to join the website and give their information.

---
+++ pages/mechanicsjoin/client/mechanicprofile.js
@@ -0,0 +1,26 @@
+Template.mechanicprofile.events({
+  "click #js-submit"(event,instance){
+    name = instance.$('#js-name').val();
+    console.log('just read '+name);
+    email = instance.$('#js-email').val();
+    console.log('just read ' +email );
+    phone = instance.$('#js-phone').val();
+    console.log('just read ' + phone);
+    iphone = instance.$('#js-iphone').val();
+    console.log('just read ' + iphone);
+    android = instance.$('#js-android').val();
+    console.log('just read ' + android);
+    imac = instance.$('#js-imac').val();
+    console.log('just read ' + imac);
+    pc = instance.$('#js-pc').val();
+    console.log('just read ' + pc);
+    this.user.name = name;
+    this.user.email = email;
+    this.user.phone = phone;
+    this.user.iphone = iphone;
+    this.user.android = android;
+    this.user.imac = imac;
+    this.user.pc = pc;
+    Profiles.update(this.user._id,this.user);
+  }
+})
