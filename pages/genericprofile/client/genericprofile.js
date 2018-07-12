9602f02cd855a7270e6b27aeef9f7f10745095a1
William Martin-Gleason <williammartingleason@gmail.com>
2018-07-12 12:04:47 -0400

    nothing

---
+++ pages/genericprofile/client/genericprofile.js
@@ -0,0 +1,21 @@
+Template.genericprofile.events({
+  "click .button": function(event,instance){
+    Meteor.call("test1",function(e,r){console.log(r)});
+    const review = $("#review").val();
+    const name = $("#name").val();
+    const rating = $("#rating").val();
+    console.log("review="+review)
+    if(review==""||rating=""||name="")
+    return
+
+    Reviews.insert({
+        review:review, name:name, rating:rating,
+    });
+  }
+});
+
+Template.profile.helpers({
+  Reviews: function() {
+    return Reviews.find().fetch()
+  }
+})
--- pages/searchresults/client/searchresults.js
+++ pages/searchresults/client/searchresults.js
@@ -1,5 +1,5 @@
 Template.searchresults.helpers({
   profiles: function(){
-    const profiles = Profiles.find({}).fetch()
+    return Profiles.find({}).fetch();
   }
 })
