792b4b83c9d53c1373e097bee1130e286d8be6e4
William Martin-Gleason <williammartingleason@gmail.com>
2018-07-13 15:44:48 -0400

    added zipcode to mechanic collection

--- pages/mechanicsjoin/client/mechanicprofile.js
+++ pages/mechanicsjoin/client/mechanicprofile.js
@@ -5,10 +5,11 @@ Template.mechanicprofile.events({
     email = instance.$('#js-email').val();
     console.log('just read ' +email );
     phone = instance.$('#js-phone').val();
+    zipcode = instance.$('#js-zipcode').val();
     education = instance.$('#js-education').val();
     console.log('just read ' + phone);
-    repairs = instance.$('#js-repairs').val();
-    Profiles.insert({name:name,owner:Meteor.userId(),email:email,phone:phone,education:education,repairs:repairs,rating:5,reviews:0});
+    /*repairs = instance.$('#js-repairs').val();*/
+    Profiles.insert({name:name,owner:Meteor.userId(),email:email,phone:phone,zipcode:zipcode,education:education,repairs:repairs,rating:5,reviews:0});
     console.log(Profiles.findOne({name:name}));
   }
 })
