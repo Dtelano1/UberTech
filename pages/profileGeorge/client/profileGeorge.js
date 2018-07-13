9027485dc45747c025361f14d99c963d8f9566ed
Max Englander <basketballdude303@gmail.com>
2018-07-12 20:31:15 -0400

    SADFASDF

--- pages/profileGeorge/client/profileGeorge.js
+++ pages/profileGeorge/client/profileGeorge.js
@@ -1,27 +1,27 @@
-Template.profile.events({
-  "click .button": function(event,instance){
-    Meteor.call("test1",function(e,r){console.log(r)});
-    const review = $("#reviewGeorge").val();
-    const name = $("#nameGeorge").val();
-    const rating = $("#js-ratingsGeorge").val();
-    console.log(`values are ${JSON.stringify([review,name,rating])}`)
-    if(review==""||rating==""||name==""){
-      alert("invalid entry")
-    }
-    else {
-      Reviews.insert({
-          review:review, name:name, rating:rating,
-      });
+//Template.profile.events({
+//  "click .button": function(event,instance){
+//    Meteor.call("test1",function(e,r){console.log(r)});
+//    const review = $("#reviewGeorge").val();
+//    const name = $("#nameGeorge").val();
+//    const rating = $("#js-ratingsGeorge").val();
+//    console.log(`values are ${JSON.stringify([review,name,rating])}`)
+//    if(review==""||rating==""||name==""){
+//     alert("invalid entry")
+//    }
+//    else {
+//      Reviews.insert({
+//          review:review, name:name, rating:rating,
+//      });
 
-    }
+//    }
 
 
 
-  }
-});
+//  }
+//});
 
-Template.profile.helpers({
-  Reviews: function() {
-    return Reviews.find().fetch()
-  }
-})
+//Template.profile.helpers({
+//  Reviews: function() {
+//    return Reviews.find().fetch()
+//  }
+//})
